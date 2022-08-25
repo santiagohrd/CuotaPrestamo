4c2afa09d384bfb7b8ca3fe38aed1017f722711f
Santiago Herrera <santiagohrd1@gmail.com>
2022-08-25 06:18:44 -0500

    Inicio de proyecto CuotaPrestamo

---
+++ src/CuotaPrestamo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CuotaPrestamo {
+    public static void main(String[] args){
+
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Programa para calcular el valor de la cuota de prestamo");
+
+    }
+}
